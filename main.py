9daa992379ee7ec37736df880bf6990d96caee19
gravitational <79793075+stabiron09@users.noreply.github.com>
2022-11-29 13:29:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
-import logging
-import logging.handlers
-import os
-
 import requests
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-logger_file_handler = logging.handlers.RotatingFileHandler(
-    "status.log",
-    maxBytes=1024 * 1024,
-    backupCount=1,
-    encoding="utf8",
-)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-logger_file_handler.setFormatter(formatter)
-logger.addHandler(logger_file_handler)
-
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-except KeyError:
-    SOME_SECRET = "Token not available!"
-    #logger.info("Token not available!")
-    #raise
-
-
-if __name__ == "__main__":
-    logger.info(f"Token value: {SOME_SECRET}")
-
-    r = requests.get('https://weather.talkpython.fm/api/weather/?city=Berlin&country=DE')
-    if r.status_code == 200:
-        data = r.json()
-        temperature = data["forecast"]["temp"]
-        logger.info(f'Weather in Berlin: {temperature}')
+import sys
+import urllib3
+import re
+import smtplib
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+y="https://www.indiainfoline.com/top-share-market-news/1"
+url= y
+x=requests.get(url, verify=False)
+z=x.text
+x=(re.search('Top 10 stocks for today', z, re.IGNORECASE))
+if x:
+    server = smtplib.SMTP('smtp.gmail.com',587)
+    server.starttls()
+    server.login('keerak0009@gmail.com', 'oywqosuhvhqxtlvy')
+    server.sendmail('keerak0009@gmail.com', 'arpitkeer11@gmail.com', 'message')
+else:
+    print("sorry")
