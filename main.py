82255b0e87a8e181f4ae5435cc17319af35112c5
gravitational <79793075+stabiron09@users.noreply.github.com>
2023-07-02 13:23:37 +0530

    main.py

--- main.py
+++ main.py
@@ -1,88 +1,180 @@
-import requests
+from cryptography.hazmat.primitives.asymmetric import ed25519
+from urllib.parse import urlparse, urlencode
+import urllib
 import json
+import requests
+import time
+import random
 import smtplib
-#INR = ['RVN', 'RLC', 'KDA', 'VBTC', 'STORJ', 'KNC', 'BUSD', 'SHIB', 'BOND', 'CRV', 'CHZ', 'CND', 'CHR',  'XEM', 'EGLD', 'XCN', 'XMR', 'GRT', 'JASMY', 'GNO', 'FLOW', 'YFII', 'HOT', 'SUSHI', 'DIA', 'MAHA', 'MANA', 'MINA', 'MASK', 'ONE', 'OXT', 'UNI', 'MCO', 'MIR', 'MKR', 'XNO', 'EOS', 'BRISE', 'GALA', 'GARI', 'SFP', 'STX', 'SNT', 'SNX', 'SXP', 'DAI', 'JST', 'DGTX', 'DYDX', 'DOGE', 'DASH', 'AAVE', 'BCH', 'BAT', 'KLAY', 'YFI', 'DOT', 'ICX', 'IMX', 'ICP', 'ALICE', 'THETA', 'NMR', 'NEO', 'NKN', 'POLYX', 'SUPER', 'LTC', 'LPT', 'LDO', 'LRC', 'VET', 'HIGH', 'TUSD', 'FIL', 'FET', 'TRB', 'TRX', 'DGB', 'DFA', 'DNT', 'ZEC', 'ZIL', 'USDT', 'USDC', 'REN', 'REQ', 'SC', 'KSM', ]
 
-USDT =['GRT', 'REN', 'REQ', 'TRB', 'TRX', 'JST', 'ZEC', 'ZIL', 'MARSH', 'FANC', 'FIDA', 'TRADE', 'LTC', 'LPT', 'LDO', 'LRC', 'ALICE', 'DFA', 'CAKE', 'CELO', 'CELR', 'COMP', 'COTI', 'TFUEL', 'POLYX', 'MATIC', 'FTM', 'FLM', 'THETA', 'QNT', '1INCH', 'GALA', 'GARI', 'SC', 'USDC', 'OGN', 'SHIB', 'STPT', 'BNB', 'BTC', 'BTT', 'SKL', 'SOL', 'BNX', 'XCAD', 'BUSD', 'BOND', 'BAND', 'PUSH', 'POWR', 'PAXG', 'OP', 'MBOX', 'WAXP', 'UNI', 'HIGH', 'TLOS', 'AAVE', 'KAVA', 'KDA', 'KNC', 'REEF', 'RUNE', 'ROSE', 'MKR', 'YFII', 'ALPACA', 'EOS', 'JASMY', 'GNO', 'INJ', 'WAVES', 'DYDX', 'DOGE', 'OMG', 'DASH', 'CRV', 'CHZ', 'CHR', 'XEM', 'XCN', 'XMR', 'STORJ', 'LINK','QTUM', 'RVN', 'RLC', 'TON', 'TWT', 'ZRX', 'HOT', 'FLOW', 'LIT', 'DCR', 'DGB', 'DIA', 'DAO', 'DAI', 'AUDIO', 'SUPER', 'DOT', 'FIL', 'FET', 'IOST', 'IOTX', 'PEOPLE', 'GLMR', 'NMR', 'NEO', 'NKN', 'NEAR', 'NEXO', 'SAND', 'EGLD', 'BCH', 'BAT', 'SFP', 'STX', 'SNT', 'SNX', 'SXP', 'BTTC', 'BRISE', 'SPELL', 'MANA', 'MINA', 'MASK', 'UMA', 'UST', 'HBAR', 'TUSD', 'ANKR', 'AVAX', 'KLAY', 'ALGO', 'ARPA', 'ATOM', 'VOLT', 'VINU', 'KSM', 'MDX', 'MTL', 'ENS', 'ENJ', 'ETN', 'EFI', 'ETH', 'ETC', 'GMT', 'ICX', 'IMX', 'ICP', 'VET', 'YFI', 'ONE', 'OXT', 'CKB', 'XTZ', 'XLM', 'XDB', 'XDC', 'XRP', 'OOKI', 'AR', 'OSMO', 'XNO', 'APE', 'ARB', 'ARV', 'ADA', 'APT', 'AXS', 'SUSHI']
+class ApiTradingClient:
 
-BTC = [ 'KSM', 'TWT','LINK', 'SAND', 'CKB', 'OP', 'APE', 'ARB', 'ADA', 'APT', 'AXS', 'MATIC', 'XRP', 'XTZ', 'XLM', 'POWR', 'PAXG', 'SPELL', 'GMT', 'FIDA','1INCH', 'MBOX', 'OMG', 'OGN', 'UMA', 'UST', 'ALPACA', 'MDX', 'MTL', 'QTUM', 'ENS', 'ENJ', 'ETN', 'ETH', 'ETC', 'IOST', 'IOTX', 'GLMR', 'SKL', 'AUDIO', 'SOL', 'QNT', 'ANKR', 'BNB', 'KAVA', 'AVAX', 'ALGO', 'ARPA', 'ATOM', 'BNX', 'PEOPLE', 'NEAR', 'OSMO', 'NEXO', 'INJ', 'AR', 'CELO', 'CAKE', 'CELR', 'COMP', 'COTI', 'WAXP', 'TFUEL', 'LIT', 'HBAR',  'WAVES', 'TLOS', 'RUNE', 'ROSE', 'FTM', 'FLM', 'ZRX','RVN', 'RLC', 'KDA', 'STORJ', 'KNC', 'BOND', 'CRV', 'CHZ', 'CND', 'CHR', 'STPT', 'BAND', 'DCR', 'XEM', 'EGLD', 'XMR', 'GRT', 'JASMY', 'GNO', 'FLOW','HOT', 'SUSHI', 'DIA', 'MANA', 'ONE', 'OXT', 'UNI', 'MCO', 'MKR', 'XNO', 'EOS', 'GALA', 'SFP', 'STX', 'SNX', 'SXP', 'DAI', 'JST', 'DYDX', 'DOGE', 'DASH', 'AAVE', 'BCH', 'BAT', 'KLAY', 'YFI', 'DOT', 'ICX', 'IMX', 'ICP', 'ALICE', 'THETA', 'NMR', 'NEO', 'NKN', 'POLYX', 'SUPER', 'LTC', 'LPT', 'LDO', 'LRC', 'VET', 'HIGH', 'TUSD', 'FIL', 'FET', 'TRB', 'TRX', 'DGB', 'DNT', 'ZEC', 'ZIL', 'REN', 'REQ']
+  secret_key = None,
+  api_key = None
 
-BNB = [ 'ZRX','LINK','SAND', 'ADA', 'AXS', 'MATIC', 'XRP', 'XTZ', 'XLM', 'POLY', 'POWR', 'PAXG', 'SPELL', 'GMT', 'FIDA', 'MBOX', 'OGN', 'ALPACA', 'MDX', 'QTUM', 'ENS', 'ENJ', 'ETC', 'IOST', 'SOL', 'QNT', 'ANKR', 'KAVA', 'BTT', 'AVAX', 'ALGO', 'ARPA', 'ATOM', 'BNX', 'PEOPLE', 'NEAR', 'INJ', 'AR', 'CAKE', 'CELR', 'COMP', 'COTI', 'WAXP', 'TFUEL', 'HBAR', 'WAVES', 'RUNE', 'FTM','RVN', 'RLC', 'CRV', 'CHZ', 'CND', 'CHR', 'STPT', 'BAND', 'CVC', 'DCR', 'XEM', 'EGLD', 'XMR', 'GNO', 'YFII', 'HOT', 'SUSHI', 'MINA', 'MASK', 'ONE', 'UNI', 'MCO', 'MKR', 'EOS', 'GALA', 'STX', 'SNX', 'SXP', 'DAI', 'JST', 'DYDX', 'DASH', 'AAVE', 'BCH', 'BAT', 'KLAY', 'YFI', 'DOT', 'ICX', 'ICP', 'THETA', 'NMR', 'NEO', 'LTC', 'LPT', 'VET', 'TUSD', 'FIL', 'FET', 'TRB', 'TRX', 'DGB', 'ZEC', 'ZIL', 'USDC', 'REN', 'SC']
+  def __init__(self, secret_key:str, api_key:str):
+        self.secret_key = secret_key
+        self.api_key = api_key
+        self.base_url = "https://coinswitch.co"
+        self.headers = {
+            "Content-Type": "application/json"
+        }
 
-ETH = ['LINK', 'SAND', 'ADA', 'APT', 'MATIC', 'XRP', 'XTZ', 'XLM', 'POWR', 'OMG', 'MTL', 'QTUM', 'ENJ', 'ETC', 'IOST', 'IOTX', 'BNB', 'ALGO', 'ARPA', 'ATOM', 'NEAR', 'NEXO', 'AR', 'COMP', 'WAVES', 'ZRX','RLC', 'KNC', 'CRV', 'CND', 'XEM', 'EGLD', 'XMR', 'GRT', 'FLOW', 'YFII', 'HOT', 'SUSHI', 'MANA', 'MCO', 'MKR', 'XNO', 'EOS', 'SNT', 'SNX', 'JST', 'DASH', 'AAVE', 'BAT', 'YFI', 'DOT', 'ICX', 'ICP', 'THETA', 'NEO', 'LTC', 'LRC', 'VET', 'TUSD', 'TRB', 'TRX', 'DNT', 'ZEC', 'ZIL', 'REN', 'REQ', 'SC']
-currency_lists = {
-	'USDT': [USDT],
-    'BTC': [BTC],
-    'ETH': [ETH],
-    'BNB': [BNB]
-}
 
-basecurrency = ['USDT','BTC', 'ETH', 'BNB']
+  def call_api(self, url: str, method: str, headers: dict = None, payload:dict = {}):
+    '''
+    make an API call on webserver and return response
 
-for base in basecurrency:
-    CURRENCY = currency_lists[base]
-    for cure in CURRENCY:
-	        for curr in cure:
-	            url1 = f'https://public.coindcx.com/market_data/orderbook?pair=l-{base}_INR'
-	            url2 = f'https://public.coindcx.com/market_data/orderbook?pair=l-{curr}_{base}'
-	            url3 = f'https://public.coindcx.com/market_data/orderbook?pair=l-{curr}_INR'
-	            response1 = requests.get(url1)
-	            response2 = requests.get(url2)
-	            response3 = requests.get(url3)
-	            if len(response1.content) > 40 and len(response2.content) > 40 and len(response3.content) > 40 :
-	            	data1 = json.loads(response1.text)
-	            	data2 = json.loads(response2.text)
-	            	data3 = json.loads(response3.text)
-	            	if  data1['timestamp'] > 1681997077935 and data2['timestamp'] > 1681997077935 and data3['timestamp'] > 1681997077935:
-	            		
-	            		
-	            		
-	            		
-	            		
-	            	
-		            	ask_quantity1 = [float(quantity) for quantity in data1['asks'].values()][0]
-		            	bid_quantity1 = [float(quantity) for quantity in data1['bids'].values()][0]
-		            	ask_quantity2 = [float(quantity) for quantity in data2['asks'].values()][0]
-		            	bid_quantity2 = [float(quantity) for quantity in data2['bids'].values()][0]
-		            	ask_quantity3 = [float(quantity) for quantity in data3['asks'].values()][0]
-		            	bid_quantity3 = [float(quantity) for quantity in data3['bids'].values()][0]
-		            	ask_price1 = [float(price) for price in data1['asks'].keys()]
-		            	lowest_ask1 = float(min(ask_price1))
-		            	bid_price1 = [float(price) for price in data1['bids'].keys()]
-		            	highest_bid1 = float(max(bid_price1))
-		            	ask_price2 = [float(price) for price in data2['asks'].keys()]
-		            	lowest_ask2 = float(min(ask_price2))
-		            	bid_price2 = [float(price) for price in data2['bids'].keys()]
-		            	highest_bid2 = float(max(bid_price2))
-		            	ask_price3 = [float(price) for price in data3['asks'].keys()]
-		            	lowest_ask3 = float(min(ask_price3))
-		            	bid_price3 = [float(price) for price in data3['bids'].keys()]
-		            	highest_bid3 = float(max(bid_price3))
-		            	amount1 = lowest_ask1*ask_quantity1
-		            	amount2 = lowest_ask2*ask_quantity2*lowest_ask1
-		            	amount3 = highest_bid3*bid_quantity3
-		            	amount = min(amount1, amount2, amount3)
-		            	inr=1000.000000
-		            	btc=0
-		            	token=0
-		            	btc = inr/lowest_ask1
-		            	token = btc/lowest_ask2
-		            	inr = token*highest_bid3
-		            	a= {curr},{base},"safe amount:", amount,"profit:" ,inr
-		            	print("\n",{curr},{base},"safe amount:", amount,"\n", "profit:" ,inr)
-		            	if inr > 1025:
-		            		subject = "Data from my program"
-		            		body = "Here is the data:\n{}".format(a)
-		            		message = f"Subject: {subject}\n\n{body}"
-		            		smtp_server = "smtp.gmail.com"
-		            		port = 587
-		            		username = "keerak0009@gmail.com"
-		            		password = "oywqosuhvhqxtlvy"
-		            		with smtplib.SMTP(smtp_server, port) as smtp:
+    Args:
+      url (str): The API url to be called
+      method (str): The API method
+      headers (dict): required headers for API call
+      payload (dict): payload for API call
+
+    Returns:
+      json: The response of the request
+    '''
+    final_headers = self.headers.copy()
+    if headers is not None:
+      final_headers.update(headers)
+    
+    response = requests.request(method, url, headers=headers, json=payload)
+    return response.json()
+
+
+  def signatureMessage(self, method: str , url: str, payload: dict):
+    '''
+      Generate signature message to be signed for given request
+
+      Args:
+        url (str): The API url to be called
+        method (str): The API method
+        payload (dict): payload for API call
+
+      Returns:
+        json: The signature message for corresponding API call
+    '''
+    message = method + url + json.dumps(payload, separators=(',', ':'), sort_keys=True)
+    return message
+
+
+  def get_signature_of_request(self, secret_key: str, request_string: str) -> str:
+    '''
+      Returns the signature of the request
+      
+      Args:
+        secret_key (str): The secret key used to sign the request.
+        request_string (str): The string representation of the request.
+
+      Returns:
+        str: The signature of the request.
+    '''
+    request_string = bytes(request_string, 'utf-8')
+    secret_key_bytes = bytes.fromhex(secret_key)
+    secret_key = ed25519.Ed25519PrivateKey.from_private_bytes(secret_key_bytes)
+    signature_bytes = secret_key.sign(request_string)
+    signature = signature_bytes.hex()
+    return signature
+
+
+  def make_request(self, method:str, endpoint:str, payload:dict = {}, params:dict = {}):
+    '''
+    Make the request to :
+      a. generate signature message
+      b. generate signature signed by secret key
+      c. send an API call with the encoded URL
+
+    Args:
+        method (str): The method to call API
+        endpoint (str): The request endpoint to make API call
+        payload (dict): The payload to make API call for POST request
+        params (dict): The params to make GET request
+
+      Returns:
+        dict: The response of the request.
+
+    '''
+    decoded_endpoint = endpoint
+    if method == "GET" and len(params)!=0:
+      endpoint += ('&', '?')[urlparse(endpoint).query == ''] + urlencode(params)
+      decoded_endpoint = urllib.parse.unquote_plus(endpoint)
+
+    signature_msg = self.signatureMessage(method, decoded_endpoint, payload)
+
+    signature = self.get_signature_of_request(self.secret_key, signature_msg)
+
+    headers = {
+        "X-AUTH-SIGNATURE" : signature,
+        "X-AUTH-APIKEY": self.api_key
+    }
+
+    url = f"{self.base_url}{endpoint}"
+
+    response = self.call_api(url, method, headers = headers, payload = payload)
+    return json.dumps(response, indent=4)
+
+
+  def check_connection(self):
+    return self.make_request("GET", "/api-trading-service/api/v1/ping")
+
+  def validate_keys(self):
+    return self.make_request("GET", "/api-trading-service/api/v1/validate/keys")
+
+  def create_order(self,payload:dict = {}):
+    return self.make_request("POST", "/api-trading-service/api/v1/order/create", payload = payload)
+
+  def cancel_order(self, payload:dict = {}):
+    return self.make_request("PUT", "/api-trading-service/api/v1/order/cancel", payload = payload)
+
+  def get_open_orders(self, params:dict = {}):
+    return self.make_request("GET", "/api-trading-service/api/v1/orders/get/open", params = params)
+
+  def get_closed_orders(self, params:dict = {}):
+    return self.make_request("GET", "/api-trading-service/api/v1/orders/get/closed", params = params)
+
+  def get_user_portfolio(self):
+    return self.make_request("GET", "/api-trading-service/api/v1/user/portfolio")
+
+  def get_24h_all_pairs_data(self, params:dict = {}):
+    return self.make_request("GET", "/api-trading-service/api/v1/ticker/24hr/all-pairs", params = params)
+
+  def get_24h_coin_pair_data(self, params:dict = {}):
+    return self.make_request("GET", "/api-trading-service/api/v1/ticker/24hr", params = params)
+
+  def get_depth(self, params:dict = {}):
+    return self.make_request("GET", "/api-trading-service/api/v1/depth", params = params)
+
+  def get_trades(self, params:dict = {}):
+    return self.make_request("GET", "/api-trading-service/api/v1/trades", params = params)
+
+  def get_candelstick_data(self, params:dict = {}):
+    return self.make_request("GET", "/api-trading-service/api/v1/getDataForCandlestick", params = params)
+  
+  def get_exchange_precision(self, payload:dict = {}):
+    return self.make_request("POST", "/api-trading-service/api/v1/exchange-precision", payload = payload)
+
+
+secret_key = "3dd4327c45784f3d8ba0ce7c55ebafd53e23e5cfeba501e531d6f39f00902e54"
+api_key = "345173bd8edc2fc60e45e6c7cd9a9e022747e98cc4a80a22dcc4e614ad24deee"
+api_trading_client = ApiTradingClient(secret_key, api_key)
+
+y=api_trading_client.get_user_portfolio()
+json_data = json.loads(y)
+data = json.loads(y); name = "Indian Rupee"; iv = next((item["invested_value"] for item in data["data"] if item["name"] == name), None); print(f"Main balance for {name}: {iv}")
+a= iv
+if iv>1200:
+	subject = "ALERT"
+	body = "the amount have been exceeded. check and sell the cryptos manually. the current portfolio amount is given below:\n{}".format(a)
+	message = f"Subject: {subject}\n\n{body}"
+	smtp_server = "smtp.gmail.com"
+	port = 587
+	username = "keerak0009@gmail.com"
+	password = "oywqosuhvhqxtlvy"
+	with smtplib.SMTP(smtp_server, port) as smtp:
 		            			smtp.starttls()
 		            			smtp.login(username, password)
 		            			smtp.sendmail('keerak0009@gmail.com', 'arpitkeer11@gmail.com', message)
-	            	else:
-	            		print('\nold data')	         
-	            else:
-		            print("\nfailed to fetch the data")
+	            
+else:
+	print("nothing to worrry about")
+
